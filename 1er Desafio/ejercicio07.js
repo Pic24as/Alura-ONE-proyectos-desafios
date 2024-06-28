fe393719a68ade29f45e508d7320e06131eb89e7
Pic24as <jarenllacho2015@gmail.com>
2024-06-28 15:20:19 -0500

    ejercicio07 resuelto

---
+++ 1er Desafio/ejercicio07.js
@@ -0,0 +1,4 @@
+//7. Declara una variable llamada mensajeDeError y asígnale el valor "¡Error! Completa todos los campos". 
+//Ahora muestra una alerta con el valor de la variable mensajeDeError .
+var mensajeDeError = "¡Error! Completa todos los campos"
+alert(mensajeDeError)
