cfbffc2b0065b7622b6bc204056866c56f82c66a
Pic24as <jarenllacho2015@gmail.com>
2024-06-28 15:37:14 -0500

    ejercicio10 resuelto

---
+++ 1er Desafio/ejercicio10.js
@@ -0,0 +1,5 @@
+//10. Ahora, si la edad es mayor o igual a 18, muestra una alerta con el mensaje "¡Puedes obtener tu licencia de conducir!".
+var edad = 18
+if( edad >= 18 ){
+    alert("¡Puedes obtener tu licencia de conducir!")
+}
