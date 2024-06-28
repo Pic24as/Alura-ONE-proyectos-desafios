dd60b6c4459463a99e27f3e4dfe444c79baf7548
Pic24as <jarenllacho2015@gmail.com>
2024-06-28 17:13:37 -0500

    ejercicio4 resuelto

---
+++ 2do Desafio/ejercicio04.js
@@ -0,0 +1,3 @@
+//4. Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+let saldoUsuario = 2000;
+alert(`saldo disponible: S/.${saldoUsuario}`)
