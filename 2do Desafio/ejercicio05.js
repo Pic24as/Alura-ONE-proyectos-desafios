c63c6b491ea47c4d4b8fd22f16c08230a6c10587
Pic24as <jarenllacho2015@gmail.com>
2024-06-28 17:39:22 -0500

    ejercicio05 resuelto

---
+++ 2do Desafio/ejercicio05.js
@@ -0,0 +1,3 @@
+//5. Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+let nombreUsuario = prompt("Ingrese su nombre")
+alert(`Bienvenido ${nombreUsuario}!!!`)
