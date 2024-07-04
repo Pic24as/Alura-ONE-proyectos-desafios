64f3ea784e4b4c76d17a9b2668b52ea03b92d0ad
Pic24as <jarenllacho2015@gmail.com>
2024-07-03 20:37:10 -0500

    ejercicio03 resuelto

---
+++ 3er Desafio/ejercicio03.js
@@ -0,0 +1,8 @@
+//3. Crea un programa de cuenta progresiva. 
+//Pide un número y cuenta desde 0 hasta ese número utilizando un bucle 'while' en la consola del navegador.
+let numcont = prompt("Ingrese su numero final: ")
+let cont = 0;
+while(cont <= numcont){
+    console.log(cont)
+    cont++;
+}
