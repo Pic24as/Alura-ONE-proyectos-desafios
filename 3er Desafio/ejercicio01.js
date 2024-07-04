34fa313c33a00e641139a5fcbf91e57c63fabe51
Pic24as <jarenllacho2015@gmail.com>
2024-07-03 20:31:05 -0500

    ejercicio01 resuelto

---
+++ 3er Desafio/ejercicio01.js
@@ -0,0 +1,6 @@
+//1. Crea un contador que comience en 1 y vaya hasta 10 usando un bucle 'while'. Muestra cada número.
+let cont = 1;
+while(cont <= 10){
+    console.log(cont);
+    cont = cont + 1;
+}
