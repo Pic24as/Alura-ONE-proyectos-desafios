a1eb952de787a91893a41f6f38cf4a09e628704b
Pic24as <jarenllacho2015@gmail.com>
2024-07-05 14:13:22 -0500

    ejercicio02 resuelto

---
+++ 4to Desafio/ejercicio02.js
@@ -0,0 +1,4 @@
+//2. Crea una variable llamada "nombre" y asígnale tu nombre. Luego, utiliza console.log para mostrar el mensaje "¡Hola, [tu nombre]!"
+// en la consola del navegador.
+let nombre = 'Samir';
+console.log(`¡Hola, ${nombre}!`);
