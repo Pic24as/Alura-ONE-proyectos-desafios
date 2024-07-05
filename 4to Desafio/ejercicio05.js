c9df26a409b05aedd24b0a11d3f2405697b806f8
Pic24as <jarenllacho2015@gmail.com>
2024-07-05 14:28:14 -0500

    ejercicio05 resuelto

---
+++ 4to Desafio/ejercicio05.js
@@ -0,0 +1,7 @@
+//5. Crea una variable llamada "valor1" y otra llamada "valor2", asignándoles valores numéricos de tu elección. 
+//Luego, realiza la suma de estos dos valores y almacena el resultado en una tercera variable llamada "resultado". 
+//Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
+let valor1 = 3;
+let valor2 = 4;
+let resultado = 3 + 4; 
+console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`);
