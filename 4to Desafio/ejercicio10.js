36dae37fa992a0a538ec4a5c3f576f162d1a3c3e
Pic24as <jarenllacho2015@gmail.com>
2024-07-05 14:47:35 -0500

    ejercicio10 resuelto

---
+++ 4to Desafio/ejercicio10.js
@@ -0,0 +1,8 @@
+//10. Crea una variable "nota" y asígnale un valor numérico. 
+//Utiliza un if-else para determinar si la nota es mayor o igual a 7 y muestra "Aprobado" o "Reprobado" en la consola.
+var nota = 6;
+if(nota >= 7){
+    console.log("Aprobado");
+}else{
+    console.log("Desaprobado");
+}
