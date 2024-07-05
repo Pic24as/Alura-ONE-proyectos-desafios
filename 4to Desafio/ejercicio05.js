b92ff5b4b9c9e4f350d6a3acd5b59e5dd4997c92
Pic24as <jarenllacho2015@gmail.com>
2024-07-05 14:31:18 -0500

    corrigiendo ejercicio05 y ejercicio06 resuelto

--- 4to Desafio/ejercicio05.js
+++ 4to Desafio/ejercicio05.js
@@ -3,5 +3,5 @@
 //Utiliza console.log para mostrar el mensaje "La suma de [valor1] y [valor2] es igual a [resultado]." en la consola.
 let valor1 = 3;
 let valor2 = 4;
-let resultado = 3 + 4; 
+let resultado = valor1 + valor2; 
 console.log(`La suma de ${valor1} y ${valor2} es igual a ${resultado}.`);
