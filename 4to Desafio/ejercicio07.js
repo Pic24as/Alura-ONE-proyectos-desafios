9d9cf027ceafb9635b0c22b3b10a29855a96cb2e
Pic24as <jarenllacho2015@gmail.com>
2024-07-05 14:35:27 -0500

    ejercicio07 resuelto

---
+++ 4to Desafio/ejercicio07.js
@@ -0,0 +1,8 @@
+//7. Pide al usuario que ingrese su edad con prompt. 
+//Con base en la edad ingresada, utiliza un if para verificar si la persona es mayor o menor de edad y muestra un mensaje apropiado en la consola.
+let edad = parseInt(prompt("ingrese su edad: "));
+if(edad >= 18 ){
+    console.log("es mayor de edad")
+} else {
+    console.log("es menor de edad")
+}
